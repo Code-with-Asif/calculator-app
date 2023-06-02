79031ffda696096abdcea0374ce2de8f5fac71dc
Code-with-Asif <asifdeveloper430@gmail.com>
2023-06-02 21:06:43 +0500

    updated

--- src/components/Output.jsx
+++ src/components/Output.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import OutputRow from "./OutputRow";
 const Output = (props) => {
   return (
-    <div>
+    <div style={{ marginTop: "10px" }}>
       <OutputRow value={props.answer} textSize={{ fontsize: "20px" }} />
       <OutputRow value={props.user} />
     </div>
